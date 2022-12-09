4f8d15515e1cfbdbb6b4ee82b65caa9f03ce9773
Mistah6909 <88503173+Mistah6909@users.noreply.github.com>
2022-12-09 13:30:25 -0600

    Update antivirus.py
    found a way to sort files and folders using walk function

--- antivirus.py
+++ antivirus.py
@@ -1,29 +1,36 @@
-#from cryptography.fernet import fernet
+#import cryptography
+#from cryptography.fernet import Fernet
+#key = Fernet.generate_key()
+#my_secret = os.environ[key 
+
 import os
 import re
-file_extensions = [".txt",".py",".jar"]
-item = None
-scanned_files = []
-folders = []
-files = []
-checked_files = []
-print("please give the directory of the file youd like to scan")
-
-def file_scan(file = None):
-  file = input()
-  scanned_files.append(os.listdir(file))
-  for file_extension in file_extensions:
-    for file in scanned_files:
-      checked_files = re.findall(str(file_extension),str(file))
-      print(checked_files)
-      print(file)
-      if file in checked_files:
-        files.append(file)
-      if file not in checked_files:
-        folders.append(file)
-    
+file_scanned = ""
+suspicion = 0
+viruses = []
+unscanned_files = []
+files= []
+folders= []
+for cwd, folders_,files_ in os.walk("virus_samples")
+def file_scan():
+  print("input the folder/file you want to be scan")
+  for cwd, folders_, files_ in os.walk(input(),topdown=False):
+    for name in files:
+      item_scanned = os.path.join(cwd, name)
+      if os.path.isfile(item_scanned):
+        print(name + " is file") 
+        files.append(item_scanned)
+      elif os.path.isfile(item_scanned) == False:
+        print(name + " is folder")
+        folders.append(item_scanned)
+def virus_compare(content="",virus="" ):
+  for file in files_:
+    content = read(file,"r")
+    for virus_ in viruses:
+      virus = read(virus,"r")
+      for code_line in enumerate(file):
+        for virus_line in enumerate(virus)
+          re_search
 file_scan()
-print(checked_files)
-print("scanned files:" + str(scanned_files))
-print("scanned folders:" + str(folders))
-print(files)
+print(files_)
+print(folders_)
