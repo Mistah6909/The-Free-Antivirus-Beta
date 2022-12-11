40d8e62b6ed7c08685fe514dde1c35fde8c48a30
Mistah6909 <88503173+Mistah6909@users.noreply.github.com>
2022-12-11 13:56:02 -0600

    Update antivirus.py

--- antivirus.py
+++ antivirus.py
@@ -1,48 +1,36 @@
-#import cryptography
-#from cryptography.fernet import Fernet
-#key = Fernet.generate_key()
-#my_secret = os.environ[key 
-
 import os
 import re
+
 file_scanned = ""
+global suspicion
 suspicion = 0
 viruses = []
 unscanned_files = []
-files= []
-folders= []
-for cwd, folders_,files_ in os.walk("virus_samples"):
-  name = os.path.join(cwd,name)
-  viruses.append
-def file_scan():
-  print("input the folder/file you want to be scan")
-  for cwd, folders_, files_ in os.walk(input(),topdown=False):
-    for name in files:
-      item_scanned = os.path.join(cwd, name)
-      if os.path.isfile(item_scanned):
-        print(name + " is file") 
-        files.append(item_scanned)
-      elif os.path.isfile(item_scanned) == False:
-        print(name + " is folder")
-        folders.append(item_scanned)
-  for folder in folders:
-     name = os.path.join() 
-def virus_compare(content="",virus="" ):
-  for file in files_:
-    content = read(file,"r")
-    for virus_ in viruses:
-      virus = read(virus,"r")
-      for code_line in enumerate(file):
-        for virus_line in enumerate(virus):
-          if re.match(virus_line,code_line) == True:
-            suspicion =+ 100/len(content.readlines())
-         
+files = []
+for cwd, folders_, files_ in os.walk("virus_sample_folder"):
+    for name in files_:
+        name = os.path.join(cwd,name)
+        viruses.append(name)
+print(viruses)
+
+def file_scan() :
+    print("input the folder/file you want to be scan")
+    for cwd, folders_, files_ in os.walk(input()):
+        for name in files_ :
+            files.append(os.path.join(cwd, name))
+
+def virus_compare(file_con="", virus_con="") :
+    for file in files:
+        file_con = open(file,"r")
+        for virus in viruses:
+            virus_con = open(virus,"r")
+            for virusline in virus_con.readlines():
+                for fileline in file_con.readlines():
+                    match = re.match(fileline,virusline)
+                    if match:
+                        suspicion =+ len(file_con.readlines())/100
+        print(f"{file} is {suspicion}% suspicious ")
 file_scan()
-print(files_)
-print(folders_)
-        for virus_line in enumerate(virus):
-          re.match(virus_line,code_line)
-         
-file_scan()
-print(files_)
-print(folders_)
+virus_compare()
+print(files)
+print(viruses)
