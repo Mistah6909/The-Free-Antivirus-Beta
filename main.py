5dcbdcfec234c3170eec370a6781eaa62558d9c5
Mistah6909 <88503173+Mistah6909@users.noreply.github.com>
2022-12-13 18:46:08 -0600

    finished prototype working on final version

--- main.py
+++ main.py
@@ -23,21 +23,9 @@
 #                       __/ |                                                        
 #                      |___/                                                                                                                                                              
 #                                   Thanks for the download!                                                                                                      
-
-
-
-
-
-
-
-
-
-
 import os
 import re
 
-file_scanned = ""
-global suspicion
 suspicion = 0
 viruses = []
 files = []
@@ -45,7 +33,6 @@ for cwd, folders_, files_ in os.walk("virus_sample_folder"):
     for name in files_:
         name = os.path.join(cwd,name)
         viruses.append(name)
-print(viruses)
 
 def file_scan() :
     print("input the folder/file you want to be scan")
@@ -53,18 +40,20 @@ def file_scan() :
         for name in files_ :
             files.append(os.path.join(cwd, name))
 
-def virus_compare(file_con="", virus_con="") :
-    for file in files:
-        file_con = open(file,"r")
-        for virus in viruses:
-            virus_con = open(virus,"r")
-            for virusline in virus_con.readlines():
-                for fileline in file_con.readlines():
-                    match = re.match(fileline,virusline)
-                    if match:
-                        suspicion =+ len(file_con.readlines())/100
-        print(f"{file} is {suspicion}% suspicious ")
+def virus_compare(virusfile="",file=""):
+    global suspicion
+    for virus in viruses:
+        virusfile = open(virus,"r")
+        for file_ in files:
+            file =  open(file_,"r")
+            for fileline in file.readlines():
+               for virusline in virusfile.readlines():
+                    comparison = re.search(virusline,fileline)
+                    if comparison:
+                        file_length = 0
+                        print(comparison)
+                        file = open(file_,"r")
+                        suspicion = suspicion + 100/len(file.readlines())
+                        print(f"{file_} is {suspicion} % suspicous")
 file_scan()
 virus_compare()
-print(files)
-print(viruses)
